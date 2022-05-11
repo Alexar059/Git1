da86ac8034f303e99c6c937b8b3991eedbbd1186
Alexar059 <300720_11@mail.ru>
2022-05-11 19:53:06 +0700

    Homework1

--- src/lesson1/lesson1.java
+++ src/lesson1/lesson1.java
@@ -4,7 +4,7 @@ public class lesson1 {
 
 
     public static void main(String[] args) {
-        System.out.println("i'm lesson1 branch!");
+        System.out.println("i'm lesson1 branch!!!!");
 
     }
 }
